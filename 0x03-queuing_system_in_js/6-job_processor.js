661710c33d866f4bcf628a41aa701a125dfa7ed0
Sesh-Gallagher <t.ntwana@gmail.com>
2024-12-05 03:16:16 +0000

    Writing the test for job creation

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Represents job processor 
+ */
+import { createQueue } from 'kue';
+
+
+const queue = createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code', function(job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
