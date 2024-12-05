c204aa565ace7e39080f93ab8d1916a136fd1d4e
Sesh-Gallagher <t.ntwana@gmail.com>
2024-12-05 01:25:34 +0000

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/**
+ * Connect to redis server via redis client
+ */
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (error, result) => {
+    console.log(result);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
