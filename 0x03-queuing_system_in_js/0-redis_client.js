3e104087d66096e1f5eec109c0df1d0bd674ce45
Sesh-Gallagher <t.ntwana@gmail.com>
2024-12-05 00:50:18 +0000

    Install a redis instance

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Connect to redis server via redis client
+ */
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
